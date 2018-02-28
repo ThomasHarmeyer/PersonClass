43e637ccc6d975ee10cdd981186deb4bc9cfecce
Andrew Doepke <andrewdoepke@outlook.com>
2018-02-27 22:22:45 -0600

    Part 2 of interesting Facts-
    Actually put Interesting facts feature into the Person class. Also- Cleaned up default constructor. The this. was not needed since it is not referencing anything else, and got rid of the void on the contstructor, void is only on methods

--- src/classes/Person.java
+++ src/classes/Person.java
@@ -1,17 +1,29 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Person {
 	private String name;
 	private double salary;
+	private ArrayList<String> funFacts = new ArrayList<>();
 
-	public void Person() {
-		this.name = "Bob";
-		this.salary = 1000000;
+	public Person() {
+		name = "Bob";
+		salary = 1000000;
+		funFacts.add("Cool Fact #1");
 	}
 
 	public void moreMoney() {
 		if (salary + 1000 < Double.MAX_VALUE) {
 			salary += 1000;
 		}
+	}
+
+	public String aboutMe() {
+		String abt = "";
+		for (int i = 0; i < funFacts.size(); i++) {
+			abt += (i + 1) + ": " + funFacts.get(i) + "\n";
+		}
+		return abt;
 	}
 }
